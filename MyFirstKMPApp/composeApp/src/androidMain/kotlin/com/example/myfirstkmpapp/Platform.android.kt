55a036913a683a644d918bee77c23f5d64586c46
Ivan Nandira <ivannandira@gmail.com>
2026-04-26 22:01:16 +0700

    Delete MyFirstKMPApp directory

--- MyFirstKMPApp/composeApp/src/androidMain/kotlin/com/example/myfirstkmpapp/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myfirstkmpapp
-
-import android.os.Build
-
-actual fun getPlatform(): Platform = Platform(
-    name = "Android ${Build.VERSION.RELEASE} (API ${Build.VERSION.SDK_INT})"
-)
--- MyFirstKMPApp/composeApp/src/desktopMain/kotlin/com/example/myfirstkmpapp/Platform.desktop.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myfirstkmpapp
-
-actual fun getPlatform(): Platform = Platform(
-    name = "Desktop (JVM ${System.getProperty("java.version")})"
-)
